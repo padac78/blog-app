52755e9c41dfbae0bf235a7ca78f98d1200a45c4
enzoob <enzo.bodin@supdevinci-edu.fr>
2022-02-08 23:01:18 +0100

    frontend new post

--- pages/new.js
+++ pages/new.js
@@ -1,66 +1,27 @@
-import Link from "next/link"
-import { useState, useEffect } from "react"
 import fetch from "isomorphic-unfetch"
 import { useRouter } from "next/router"
 
 const NewPost = () => {
-  const [form, setForm] = useState({ title: "", text: "" })
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const [errors, setErrors] = useState({})
   const router = useRouter()
-
-  useEffect(() => {
-    if (isSubmitting) {
-      if (Object.keys(errors).length === 0) {
-        createposts()
-        alert("Publication créée")
-      } else {
-        setIsSubmitting(false)
-      }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = {
+      title: e.target.title.value,
+      text: e.target.text.value,
     }
-  }, [errors])
-
-  const createposts = async () => {
     try {
-      const res = await fetch("http://localhost:3000/api/posts", {
+      fetch("http://localhost:3000/api/posts", {
         method: "POST",
         headers: {
-          Accept: "application/json",
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(form),
+        body: JSON.stringify(data),
       })
+      alert("Publication posté")
       router.push("/")
     } catch (error) {
       console.log(error)
     }
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    let errs = validate()
-    setErrors(errs)
-    setIsSubmitting(true)
-  }
-
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    })
-  }
-
-  const validate = () => {
-    let err = {}
-
-    if (!form.title) {
-      err.title = "Le titre est requis"
-    }
-    if (!form.text) {
-      err.text = "Le contenu est requis"
-    }
-
-    return err
   }
 
   return (
@@ -68,12 +29,14 @@ const NewPost = () => {
       <h1>Créer une publication</h1>
       <div>
         <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Titre :</label>
           <input
             type="text"
-            name="titre"
+            name="title"
             className="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-2 placeholder-slate-400 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
             placeholder="Vous devez saisir un titre"
           />
+          <label htmlFor="text">Texte :</label>
           <input
             type="textarea"
             name="text"
