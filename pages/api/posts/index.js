5937546136c28b3e175ec1ed8b0d666818ac6edd
AadamRf <rouf.aadam@gmail.com>
2022-02-08 12:28:54 +0100

    update create delete posts

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -16,6 +16,15 @@ const getPosts = async (req, res) => {
         res.status(404).json({ message: error.message })
         break
       }
+    case "POST":
+      try {
+        const post = await Post.create(req.body)
+        res.status(201).json({ success: true, data: post })
+        break
+      } catch (error) {
+        res.status(400).json({ success: false })
+        break
+      }
     default:
       res.status(404).json({ message: "error" })
       break
