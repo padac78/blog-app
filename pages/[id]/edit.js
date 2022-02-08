9718dbfc371d2e5f8e748196b8c070aa5693a831
enzoob <enzo.bodin@supdevinci-edu.fr>
2022-02-08 23:01:27 +0100

    frontend edit post

--- pages/[id]/edit.js
+++ pages/[id]/edit.js
@@ -3,71 +3,72 @@ import { useState, useEffect } from "react"
 import fetch from "isomorphic-unfetch"
 import { useRouter } from "next/router"
 
-const EditPost = ({ posts }) => {
-  const [form, setForm] = useState({ title: posts.title, text: posts.text })
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const [errors, setErrors] = useState({})
+const EditPost = ({ post }) => {
   const router = useRouter()
-
-  useEffect(() => {
-    if (isSubmitting) {
-      if (Object.keys(errors).length === 0) {
-        updatePost()
-      } else {
-        setIsSubmitting(false)
-      }
+  const [data, setData] = useState({
+    title: post.title,
+    text: post.text,
+  })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = {
+      title: e.target.title.value,
+      text: e.target.text.value,
     }
-  }, [errors])
-
-  const updatePost = async () => {
     try {
-      const res = await fetch(
-        `http://localhost:3000/api/posts/${router.query.id}`,
-        {
-          method: "PUT",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(form),
-        }
-      )
-      router.push("/")
+      fetch(`http://localhost:3000/api/posts/${post._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+      alert("Publication mise à jour")
+      router.push(`/${post._id}`)
     } catch (error) {
       console.log(error)
     }
   }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    let errs = validate()
-    setErrors(errs)
-    setIsSubmitting(true)
-  }
-
   const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
+    const { name, value } = e.target
+    setData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      }
     })
   }
 
-  const validate = () => {
-    let err = {}
-
-    if (!form.title) {
-      err.title = "Le titre est requis"
-    }
-    if (!form.text) {
-      err.text = "Le contenu est requis"
-    }
-
-    return err
-  }
-
+  const { title, text } = data
   return (
     <div className="form-container">
       <h1>Editer une publication</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Titre :</label>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleChange}
+            className="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-2 placeholder-slate-400 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+          />
+          <label htmlFor="text">Texte :</label>
+          <input
+            type="textarea"
+            name="text"
+            value={text}
+            onChange={handleChange}
+            className="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-2 placeholder-slate-400 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+          />
+          <button
+            type="submit"
+            className="rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+          >
+            Editer
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
@@ -76,7 +77,7 @@ EditPost.getInitialProps = async ({ query: { id } }) => {
   const res = await fetch(`http://localhost:3000/api/posts/${id}`)
   const { data } = await res.json()
 
-  return { posts: data }
+  return { post: data }
 }
 
 export default EditPost
